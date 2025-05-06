6363c0416eee270e343d08ffcdaafa09d8c46a32
chaima982 <chaimaawala@email.com>
2025-05-06 20:54:08 +0100

    new

--- db.php
+++ db.php
@@ -1,14 +1,26 @@
 <?php
 $host = 'localhost';
 $dbname = 'recommendation';
-$username = 'root'; // change si besoin
-$password = '';     // change si tu as un mot de passe
+$username = 'root';
+$password = '';     
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    // Affiche les erreurs PDO
+   
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
+
+$height = $_POST['height'];
+$weight = $_POST['weight'];
+$bmi = $_POST['bmi'];
+$status = $_POST['status'];
+
+// Insertion dans la base de données
+$stmt = $pdo->prepare("INSERT INTO imc_results (height, weight, bmi, status) VALUES (?, ?, ?, ?)");
+$stmt->execute([$height, $weight, $bmi, $status]);
+
+echo "success";
+
+}catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
 ?>
